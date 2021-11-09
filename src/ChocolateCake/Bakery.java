cda2079f09c75c0578f269b17303ef48c73ee070
Ady <yethihaaung@my.jcu.edu.au>
2021-11-09 11:49:55 +0700

    cake

---
+++ src/ChocolateCake/Bakery.java
@@ -0,0 +1,21 @@
+package ChocolateCake;
+
+public class Bakery {
+    CupCake bakeryCupcake;
+    double price;
+    String giveTotal;
+
+    public Bakery(CupCake bakeryCake, double cakePrice){
+        this.bakeryCupcake = bakeryCake;
+        this.price = cakePrice;
+
+        giveTotal = "The " + bakeryCake.flavor + " cupcake is $ " + price;
+    }
+
+    public static void main(String[] args) {
+        CupCake chocolate = new CupCake("Chocolate", true);
+        Bakery newCake = new Bakery(chocolate, 22);
+
+        System.out.println(newCake.giveTotal);
+    }
+}
---
+++ src/ChocolateCake/CupCake.java
@@ -0,0 +1,11 @@
+package ChocolateCake;
+
+public class CupCake {
+    String flavor;
+    boolean sprinkles;
+
+    public CupCake(String flavorType, boolean sprinklesTop){
+        this.flavor = flavorType;
+        this.sprinkles = sprinklesTop;
+    }
+}
